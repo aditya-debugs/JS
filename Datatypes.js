3db65fc5f76babfc8a830a7544d99f88cea44d2c
aditya-debugs <adityaofficial1641@gmail.com>
2024-04-07 19:04:40 +0530

    Datatypes

---
+++ Datatypes.js
@@ -0,0 +1,22 @@
+"use strict" // using this will treat all the JS code to newest JS version
+//alert(3+3) 
+//This will give error because its not browser, alert is used to prompt a pop-up window in browser
+
+let name= "Aditya"
+let age= 18
+let isLoggedIn= false
+
+//number => 2 to power 53
+// bigint
+// string
+// boolean => true/false
+// null => standalone value (object)
+// undefined => 
+// symbol => uniqueness
+
+
+// object
+
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined)
