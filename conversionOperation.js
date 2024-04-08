4e4dd2edacf7ffc4d6fd70c5c9397d603acf185c
aditya-debugs <adityaofficial1641@gmail.com>
2024-04-08 11:14:06 +0530

    Conversion of DataTypes

--- conversionOperation.js
+++ conversionOperation.js
@@ -1,8 +1,26 @@
-let score = 33
+let score = "adi"
 
-console.log(typeof score);
-console.log(typeof(score));
+//console.log(typeof score);
+//console.log(typeof(score));
 
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN - Not A Number
+// true => 1 ; false => 0
+
+let isLoggedIn = ""
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true ; 0 => false
+// "" => false
+// "aditya" => true
+
+let num = 33
+
+let stringNumber = String(num)
+console.log(stringNumber);
+console.log(typeof stringNumber);
