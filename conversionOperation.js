545d0ad867a7caef47434a006f72f625368f937d
aditya-debugs <adityaofficial1641@gmail.com>
2024-05-05 20:35:34 +0530

    new

--- conversionOperation.js
+++ conversionOperation.js
@@ -22,5 +22,41 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let num = 33
 
 let stringNumber = String(num)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *****OPERATIONS*****
+
+let value= 3
+let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " aditya"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1+ 2 + "2");
+
+// console.log( (3+4) * 5 % 3);
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1=num2=num3= 2+2
+
+let gamecounter =100
+gamecounter++
+console.log(gamecounter);
